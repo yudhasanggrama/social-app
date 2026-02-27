3b9160ec73bba5daa1e0a0749749df57713282e0
Yudha <yudhasanggrama@gmail.com>
2026-02-28 05:41:10 +0700

    feat: try to deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,14 +5,13 @@ import { connectRedis } from "./lib/redis";
 const PORT = process.env.PORT || 3000; 
 
 async function bootstrap() {
-  try {
-    await connectRedis();
-    console.log("✅ Redis connected");
-  } catch (e) {
-    console.error("Redis failed to connect:", e);
-  }
+  // try {
+  //   await connectRedis();
+  //   console.log("✅ Redis connected");
+  // } catch (e) {
+  //   console.error("Redis failed to connect:", e);
+  // }
 
-  // TAMBAHKAN "0.0.0.0" di sini agar bisa diakses secara publik
   server.listen(Number(PORT), "0.0.0.0", () => {
     console.log(`🚀 Server running on port ${PORT}`);
     console.log(`API   : https://social-app-production-75eb.up.railway.app/api/v1`);
